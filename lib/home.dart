da79baf7af00b6ef40e9ff6acc28292ee2a10499
ritesh prasad nandan <prasadritesh261@gmail.com>
2023-04-19 15:59:57 +0530

    ui part of changes all textform field , buttons,

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
   @override
-  _SignUpScreenState createState() => _SignUpScreenState();
+  SignUpScreenState createState() => SignUpScreenState();
 }
 
-class _SignUpScreenState extends State<SignUpScreen> {
+class SignUpScreenState extends State<SignUpScreen> {
   final _formKey = GlobalKey<FormState>();
 
   final _emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
@@ -18,6 +20,11 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   void _submitForm() {
     if (_formKey.currentState!.validate()) {
+      debugPrint(
+        _name,
+      );
+      debugPrint(_email);
+      debugPrint(_password);
       // Valid form, do something
     }
   }
@@ -29,68 +36,183 @@ class _SignUpScreenState extends State<SignUpScreen> {
         title: const Text('Sign Up'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(15.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Enter Your Name'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Name is required';
-                  }
-                  if (value.length < 4) {
-                    return 'Too Short';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _name = value;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Email is required';
-                  } else if (!_emailRegex.hasMatch(value)) {
-                    return 'Invalid email format';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _email = value;
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Password is required';
-                  } else if (!_passwordRegex.hasMatch(value)) {
-                    return 'Password must be at least 8 characters long and contain at least 1 uppercase letter'
-                        '1 lowercase letter, and one number';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _password = value;
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: ElevatedButton(
-                  onPressed: _submitForm,
-                  child: const Text('Sign Up'),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: CircleAvatar(
+                      radius: 50.0,
+                      backgroundImage: NetworkImage(
+                          'https://www.tekkiwebsolutions.com/wp-content/uploads/retail-ecommerce-min-1.webp'),
+                      backgroundColor: Colors.transparent,
+                    ),
+                  ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Create an Account , Its Free',
+                        style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                      )
+                    ],
+                  ),
+                ),
+                createTextField(
+                    maxLength: 50,
+                    labelText: 'Name',
+                    hintText: 'Ritesh',
+                    prefixIcon: Icons.person_add,
+                    keyboardType: TextInputType.name,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Name is required';
+                      }
+                      if (value.length < 4) {
+                        return 'Too Short';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _name = value;
+                    }),
+                const SizedBox(
+                  height: 8,
+                ),
+                createTextField(
+                  maxLength: 50,
+                  labelText: 'Email Adress',
+                  hintText: 'rpn@gamil.com',
+                  prefixIcon: Icons.email,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email is required';
+                    } else if (!_emailRegex.hasMatch(value)) {
+                      return 'Invalid email format';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _email = value;
+                  },
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                createTextField(
+                  labelText: 'Password',
+                  hintText: "********",
+                  obscureText: true,
+                  suffixicon: Icons.lock,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password is required';
+                    } else if (!_passwordRegex.hasMatch(value)) {
+                      return 'Password must be at least 8 characters long and contain at least 1 uppercase letter'
+                          '1 lowercase letter, and one number';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _password = value;
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 50.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Container(
+                          height: 50,
+                          width: 300,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40)),
+                          child: ElevatedButton(
+                            onPressed: _submitForm,
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('Dont have an account ?'),
+                      Text(
+                        ' Sign Up',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 17,
+                            color: Colors.blue),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget createTextField({
+    required String labelText,
+    required String hintText,
+    IconData? suffixicon,
+    IconData? prefixIcon,
+    TextInputType keyboardType = TextInputType.text,
+    bool obscureText = false,
+    FormFieldValidator<String>? validator,
+    FormFieldSetter<String>? onSaved,
+    ValueChanged<String>? onChanged,
+    int? maxLength,
+  }) {
+    return TextFormField(
+      maxLength: maxLength,
+      keyboardType: keyboardType,
+      cursorColor: Colors.black,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+        labelText: labelText,
+        hintText: hintText,
+        prefixIcon: prefixIcon != null ? Icon(prefixIcon) : null,
+        suffixIcon: suffixicon != null ? Icon(suffixicon) : null,
+      ),
+      validator: validator,
+      onSaved: onSaved,
+      onChanged: onChanged,
     );
   }
 }
